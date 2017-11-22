695adde86114cf845c634c3c48609b8afaf63fac
Programito <32197103+Programito@users.noreply.github.com>
2017-11-22 17:46:01 +0100

    test de comprobacion de rangos de la columna

--- workspace0/Bingo/src/com/bingo/test/BingoTest.java
+++ workspace0/Bingo/src/com/bingo/test/BingoTest.java
@@ -5,9 +5,59 @@ import java.util.ArrayList;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.bingo.utils.utils;
+
 
 public class BingoTest {
+	
+
 	@Test
+	public void betweenMinMaxTest(){
+		//valor,minimo,maximo
+		Assert.assertEquals(true, utils.betweenMinMax(12, 1, 15));
+		Assert.assertEquals(false, utils.betweenMinMax(20, 12, 18));
+		Assert.assertEquals(false, utils.betweenMinMax(1, 15, 20));
+	}
+	
+	@Test
+	public void checkArrayMinMaxTest(){
+		int[] valores={16,17,18,19,20,21}; 
+		Assert.assertEquals(true, utils.checkArrayMinMax(valores, 16,30));
+		Assert.assertEquals(false, utils.checkArrayMinMax(valores, 16,20));
+		Assert.assertEquals(false, utils.checkArrayMinMax(valores, 17,30));
+		valores[3]=31;
+		Assert.assertEquals(false, utils.checkArrayMinMax(valores, 16,30));
+	}
+	
+	@Test
+	public void getColTest(){
+		int[][] card={{3,17,35,54,61},{8,28,41,48,68},{4,23,-1,52,70},{7,29,37,55,74},{11,19,33,50,73}};
+		Assert.assertArrayEquals(new int[] {3,8,4,7,11},utils.getCol(card, 0));
+		Assert.assertArrayEquals(new int[] {17,28,23,29,19},utils.getCol(card, 1));
+		Assert.assertArrayEquals(new int[] {35,41,37,33},utils.getCol(card, 2));
+		Assert.assertArrayEquals(new int[] {54,48,52,55,50},utils.getCol(card, 3));
+		Assert.assertArrayEquals(new int[] {61,68,70,74,73},utils.getCol(card, 4));
+	}
+	
+	@Test
+	public void checkMinMaxTest(){
+		int[][] card={{3,17,35,54,61},{8,28,41,48,68},{4,23,-1,52,70},{7,29,37,55,74},{11,19,33,50,73}};
+		Assert.assertTrue(utils.checkMinMax(card, 0));
+		Assert.assertTrue(utils.checkMinMax(card, 1));
+		Assert.assertTrue(utils.checkMinMax(card, 2));
+		Assert.assertTrue(utils.checkMinMax(card, 3));
+		Assert.assertTrue(utils.checkMinMax(card, 4));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*@Test
 	public void CallingNumberTest(){
 		CardT cardT=new CardT();
 		int max=15;
@@ -57,7 +107,7 @@ public class BingoTest {
 	}
 	
 	public static class CardT{
-		int[][] card= {{-1,8,4,7,11},{17,-1,23,28,19},{35,41,-1,37,33},{54,48,52,-1,50},{61,68,70,74,-1}};
+		int[][] card= {{-1,8,15,7,11},{17,-1,23,20,19},{35,41,-1,37,33},{54,48,52,-1,50},{61,68,70,74,-1}};
 		
 		public CardT(){
 			
@@ -75,7 +125,7 @@ public class BingoTest {
 			return card.length;
 		}
 		
-	}
+	}*/
 	
 	
 	
